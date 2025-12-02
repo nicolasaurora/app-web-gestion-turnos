2fe0378c76bd7457b5ba6381f29c11cb6b0925e9
nicolasaurora <nicolas.aurora5@gmail.com>
2025-12-02 12:25:02 -0300

    Configuracion inicial del proyecto Gestion de turnos con Java + SpringBoot

---
+++ src/main/java/com/agendadakota/agendadakota/AgendadakotaApplication.java
@@ -0,0 +1,13 @@
+package com.agendadakota.agendadakota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadakotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadakotaApplication.class, args);
+	}
+
+}
